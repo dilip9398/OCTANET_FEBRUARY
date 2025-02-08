34efe2fe3528c840301e0e728d01a747c092ba2c
dilip9398 <138967561+dilip9398@users.noreply.github.com>
2025-02-08 22:21:42 +0530

    landing page

--- Gym Landing page/app.js
+++ Gym Landing page/app.js
@@ -7,3 +7,47 @@ $(document).ready(function () {
         }
     });
 });
+
+
+
+// counting the number animation
+function animateCount(countElement) {
+    const targetNumber = +countElement.getAttribute('data-target');
+    const duration = +countElement.getAttribute('data-duration');
+    let currentNumber = 0;
+    // incerases the value
+    const incrementStep = targetNumber / (duration / 10);
+    const interval = setInterval(() => {
+        currentNumber += incrementStep;
+
+        // Stops when it reaches the target
+        if (currentNumber >= targetNumber) {
+            currentNumber = targetNumber;
+            clearInterval(interval);
+        }
+
+        countElement.textContent = Math.round(currentNumber) + "+";
+    }, 10);
+}
+
+// when we scroll down and see the section we see the animation
+document.addEventListener("DOMContentLoaded", () => {
+    const counts = document.querySelectorAll('.count');
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+
+                animateCount(entry.target);
+
+
+                observer.unobserve(entry.target);
+            }
+        });
+    }, {
+        threshold: 0.5
+    });
+
+    counts.forEach(count => observer.observe(count));
+});
+
+// End of counting
